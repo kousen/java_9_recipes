a1e7179b77f4d8e0ff6b9ed2b4861b353df92084
Ken Kousen <ken.kousen@kousenit.com>
2017-12-21 09:41:50 -0500

    Clean up imports

--- server/src/main/java/com/oreilly/suppliers/NamesSupplier.java
+++ server/src/main/java/com/oreilly/suppliers/NamesSupplier.java
@@ -1,23 +1,11 @@
 package com.oreilly.suppliers;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import java.util.function.Supplier;
-import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 public class NamesSupplier implements Supplier<Stream<String>> {
-    private Logger logger = Logger.getLogger(this.getClass().getName());
-
     private List<String> strings = Arrays.asList("Londo", "Vir", "G'Kar", "Na'Toth",
                                                  "Delenn", "Lennier", "Kosh");
 
--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -1,4 +1,3 @@
 module com.oreilly.suppliers {
-    requires java.logging;
     exports com.oreilly.suppliers;
 }
