32ff5ee31dda2d932cec07acf5b13c04d3d5fa3b
Ken Kousen <ken.kousen@kousenit.com>
2018-04-07 12:15:42 -0500

    Update var test examples

---
+++ src/test/java/lvti/VarTypeTest.java
@@ -0,0 +1,41 @@
+package lvti;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VarTypeTest {
+    @Test
+    void inferString() {
+        var s = "Hello, World!";
+        assertEquals(String.class, s.getClass());
+    }
+
+    @Test
+    void inferInteger() {
+        var num = 3;
+        assertTrue(Integer.valueOf(num) instanceof Integer);
+    }
+
+    @Test
+    void inferListOfString() {
+        var strings = List.of("this", "is", "a", "list", "of", "strings");
+        assertTrue(strings instanceof List);
+    }
+
+    @Test
+    void inferArrayListOfString() {
+        var strings = new ArrayList<String>();
+        assertEquals(ArrayList.class, strings.getClass());
+    }
+
+    @Test
+    void dontDoThis() {
+        var var = "var";
+        assertEquals(String.class, var.getClass());
+    }
+}
